e2b69cac19f4b4c600cd2985c08959092d5acb5d
Peng Xiao <xiaoquwl@gmail.com>
2020-09-14 23:25:58 +0200

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# Terraform
+
+Terraform helloworld
+
+```
+$ terraform int
+$ terraform plan
+$ terraform apply
+```
+
+az vm image terms accept --urn cisco:cisco-csr-1000v:16_12-payg-sec:16.12.120190816
--- main.tf
+++ main.tf
@@ -7,15 +7,14 @@ provider "azurerm" {
 resource "azurerm_resource_group" "terraform_demo" {
   name     = "terraform-demo"
   location = "West Europe"
-
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terraform_demo.location
-    resource_group_name = azurerm_resource_group.terraform_demo.name
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.terraform_demo.location
+  resource_group_name = azurerm_resource_group.terraform_demo.name
 }
 
 # Create subnet
@@ -55,9 +54,9 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                      = "myNIC"
-  location                  = azurerm_resource_group.terraform_demo.location
-  resource_group_name       = azurerm_resource_group.terraform_demo.name
+  name                = "myNIC"
+  location            = azurerm_resource_group.terraform_demo.location
+  resource_group_name = azurerm_resource_group.terraform_demo.name
 
   ip_configuration {
     name                          = "myNICConfg"
@@ -75,13 +74,18 @@ resource "azurerm_network_interface_security_group_association" "nic-nsg" {
 
 
 # Create a Linux virtual machine
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "csr_router" {
   name                  = "myTFVM"
   location              = azurerm_resource_group.terraform_demo.location
   resource_group_name   = azurerm_resource_group.terraform_demo.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
 
+  plan {
+    publisher = "cisco"
+    product   = "cisco-csr-1000v"
+    name      = "16_12-payg-sec"
+  }
   storage_os_disk {
     name              = "myOsDisk"
     caching           = "ReadWrite"
@@ -90,10 +94,10 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    version   = "latest"
+    publisher = "cisco"
+    offer     = "cisco-csr-1000v"
+    sku       = "16_12-payg-sec"
+    version   = "16.12.120190816"
   }
 
   os_profile {
