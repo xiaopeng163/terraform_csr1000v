e4f0d8157dabcf472ec42a96d6a3248eda3ff999
Peng Xiao <xiaoquwl@gmail.com>
2020-10-15 09:57:22 +0200

    test

--- vars.tf
+++ vars.tf
@@ -7,3 +7,14 @@ variable "admin_password" {
   type        = string
   description = "Password must meet Azure complexity requirements"
 }
+
+variable "vm_instance_name" {
+  type        = list(string)
+  description = "vm name list"
+  default     = ["csr1000v_r1", "csr1000v_r2"]
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "terraform_demo"
+}
